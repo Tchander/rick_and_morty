dcc39665bc2835f7ca104af08e6a8dde99ca4ae8
Tchander <evgeniy.tchander@yandex.ru>
2021-10-06 21:01:57 +0300

    main add vue-persist

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,15 @@ import Vuex from "vuex";
 import characters from "./modules/characters";
 
 Vue.use(Vuex);
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 export default new Vuex.Store({
   modules: {
     characters,
   },
+  plugins: [vuexLocal.plugin],
 });
